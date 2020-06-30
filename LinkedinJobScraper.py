45d20c13d31806398456aa936e852d9422fdb590
SZCodes <sophie14159@gmail.com>
2020-06-30 15:52:11 -0700

    30062020 LinkedinJobScraper
    - Fixed issue where click was interepted by messaging pop-up
    - Code loops through all jobs listed in project plan
    - Data can be written into text file: JobPostingsData

--- LinkedinJobScraper.py
+++ LinkedinJobScraper.py
@@ -1,7 +1,7 @@
 '''
 TO DO:
-1. Click intercepted by messaging pop-up
-2. Save data to txt file
+x Click intercepted by messaging pop-up
+x Save data to txt file
 3. Track data daily
 
 Note: Job postings are in Canada
@@ -9,6 +9,7 @@ Note: Job postings are in Canada
 '''
 
 import os, random, sys, time
+from datetime import date
 from urllib.parse import urlparse
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -32,24 +33,40 @@ elementID.send_keys(password)
 
 elementID.submit()
 
-
-browser.get('https://www.linkedin.com/jobs/?showJobAlertsModal=false')
-
 job_list = ['computer support technician', 'digital marketer', 'front-end developer', 'security specialist', 'ml engineer', 'marketing analyst', 'hr analyst', 'software engineer', 'data scientist']
 job_postings = {}
+message_click = True
 
-jobID = browser.find_element_by_id('jobs-search-box-keyword-id-ember19')
 for job in job_list:
+    #Access job
+    browser.get('https://www.linkedin.com/jobs/?showJobAlertsModal=false')
+    jobID = browser.find_element_by_id('jobs-search-box-keyword-id-ember18')
     jobID.send_keys(job)
+    if message_click:
+        messaging = browser.find_element_by_class_name('msg-overlay-bubble-header')
+        messaging.click()
+        message_click = False
     search = browser.find_element_by_class_name('jobs-search-box__submit-button')
     search.click()
+
+    SCROLL_PAUSE_TIME = 5
+
+    #Could condense, not sure of function
+    last_height = browser.execute_script('return document.body.scrollHeight')
+    for i in range(3):
+        browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        time.sleep(SCROLL_PAUSE_TIME)
+        new_height = browser.execute_script('return document.body.scrollHeight')
+        last_height = new_height
 
     src = browser.page_source
     soup = BeautifulSoup(src, 'lxml')
 
-    results = soup.find('div', {'class' : 'jobs-search-two-pane__title-heading'})
-    results = int(results.find('small', {'class' : 'display-flex'}).get_text().strip().split()[0])
+    results = soup.find('small', {'class': 'display-flex t-12 t-black--light t-normal'}).get_text().strip().split()[0]
+    results = int(results.replace(',', ''))
 
     job_postings[job] = results
-
-print(job_postings)
+    today = date.today()
+    data = open("JobPostingsData", "a")
+    data.write("%s %s %s\n" % (today, job, results))
+    data.close()
